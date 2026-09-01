6519835f9201c9b2197af7e57ed5aa44b837dac4
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2026-09-01 13:18:40 +0000

    feat: implement in-memory to-dos create, read, and delete

--- Api/Features/ToDos/ToDosController.cs
+++ Api/Features/ToDos/ToDosController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,21 +8,47 @@ namespace Api.Features.ToDos;
 [Route("to-dos")]
 public class ToDosController : ControllerBase
 {
+  private static long _nextToDoId = 0;
+  private static readonly ConcurrentDictionary<long, ToDo> ToDos = new();
+
   [HttpPost]
-  public Task<CreateToDoResponse> CreateToDoAsync([Required] [FromBody] CreateToDoRequest createToDoRequest)
+  public Task<CreateToDoResponse> CreateToDoAsync(
+    [Required] [FromBody] CreateToDoRequest createToDoRequest
+  )
   {
-    throw new NotImplementedException();
+    var newToDo = new ToDo
+    {
+      Id = Interlocked.Increment(ref _nextToDoId),
+      Name = createToDoRequest.Name,
+    };
+
+    ToDos[newToDo.Id] = newToDo;
+
+    return Task.FromResult(
+      new CreateToDoResponse() { NewToDoId = newToDo.Id }
+    );
   }
 
   [HttpGet]
   public Task<GetToDosResponse> GetToDosAsync()
   {
-    throw new NotImplementedException();
+    return Task.FromResult(
+      new GetToDosResponse
+      {
+        ToDos = ToDos
+          .Values.Select(x => new ToDoDto { Id = x.Id, Name = x.Name })
+          .ToList(),
+      }
+    );
   }
 
   [HttpDelete]
-  public Task<DeleteToDoResponse> DeleteToDoAsync([Required] [FromQuery] long toDoId)
+  public Task<DeleteToDoResponse> DeleteToDoAsync(
+    [Required] [FromQuery] long toDoId
+  )
   {
-    throw new NotImplementedException();
+    return Task.FromResult(
+      new DeleteToDoResponse { IsDeleted = ToDos.Remove(toDoId, out _) }
+    );
   }
 }
