b2f9d9775ab49d3968bb8221eecd8b6adf2706c8
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2026-09-09 14:57:35 +0000

    test: add GetStatus_FiveDaysAgoIsNew

---
+++ Core/Entities/ToDoTests.cs
@@ -0,0 +1,23 @@
+using Moq;
+using Xunit;
+
+namespace Core.Entities;
+
+public class ToDoTests
+{
+  [Fact]
+  public async Task GetStatus_FiveDaysAgoIsNew()
+  {
+    var dt = new DateTime(2026, 01, 02, 03, 04, 05, 678, DateTimeKind.Utc);
+
+    var toDo = new ToDo { Name = "First", CreatedAtUtc = dt };
+
+    var dateTimeProviderMock = new Mock<IDateTimeProvider>();
+
+    dateTimeProviderMock.Setup(x => x.UtcNow).Returns(dt.AddDays(5));
+
+    var status = toDo.GetStatus(dateTimeProviderMock.Object);
+
+    Assert.Equal(ToDoStatus.New, status);
+  }
+}
